99bc57657821840daf08aa2fdd4c6c66b990695a
gelu@codeindustry.net <gelu@codeindustry.net>
2023-01-28 16:44:38 +0200

    added UI part using powershell

--- IdentityServer4Integration/Program.cs
+++ IdentityServer4Integration/Program.cs
@@ -11,15 +11,24 @@ builder.Services.AddIdentityServer()
                 .AddInMemoryClients(InMemoryConfig.GetClients())
                 .AddDeveloperSigningCredential(); // not something that we want to use in a production environment;
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseStaticFiles();
+app.UseRouting();
 
 app.UseIdentityServer();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.MapControllers();
 
